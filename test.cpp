18efe95dc89e001b3a13b39f3cf9a5a7dc796e31
tellwy <54048064+tellwy@users.noreply.github.com>
2019-08-13 10:29:02 +0900

    Update test.cpp
    I try to add the new file in github repo in code review course.

--- test.cpp
+++ test.cpp
@@ -1,9 +1,34 @@
-#include <stdio.h>
+#include "priority_queue.h"
 
-int main()
-{
-  int i,j;
-  i=j=0;
-  
-  return 1;
+template <>
+templatePriorityQueue<int>::templatePriorityQueue(){
+}
+
+template <>
+templatePriorityQueue<int>::~templatePriorityQueue(){
+}
+
+template <>
+bool templatePriorityQueue<int>::empty() const{
+        return storage.empty();
+}
+
+template <>
+int templatePriorityQueue<int>::size() {
+        return storage.size();
+}
+
+template <>
+const int& templatePriorityQueue<int>::top() const{
+        return storage.top();
+}
+
+template <>
+void templatePriorityQueue<int>::pop(){
+        storage.pop();
+}
+
+template <>
+void templatePriorityQueue<int>::push(const int& val){
+        storage.push(val);
 }
